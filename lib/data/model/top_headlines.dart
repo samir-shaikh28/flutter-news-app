a2870fa12bb9e884e99fbac90e9d7b5740d70831
Samir Shaikh <samirshaikh@samirs-MacBook-Pro.local>
2021-02-20 14:29:39 +0530

    POJO  created for topHeadlines API response

--- lib/data/model/top_headlines.dart
+++ lib/data/model/top_headlines.dart
@@ -1,1 +1,45 @@
+class TopHeadlinesModel {
+  String status;
+  int totalResults;
+  List<Article> tempArticles = [];
 
+  TopHeadlinesModel.fromJson(Map<String, dynamic> parsedJson) {
+    status = parsedJson['status'];
+    totalResults = parsedJson['totalResults'];
+    for (int i = 0; i < parsedJson['articles'].length; i++) {
+      Article article = Article(parsedJson['articles'][i]);
+      tempArticles.add(article);
+    }
+  }
+}
+
+class Article {
+  Source source;
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+
+  Article(article) {
+    source = Source(article['source']);
+    author = article['author'];
+    title = article['title'];
+    description = article['description'];
+    url = article['url'];
+    urlToImage = article['urlToImage'];
+    publishedAt = article['publishedAt'];
+    content = article['content'];
+  }
+}
+
+class Source {
+  String id;
+  String name;
+  Source(source) {
+    id = source["id"];
+    name = source["name"];
+  }
+}
