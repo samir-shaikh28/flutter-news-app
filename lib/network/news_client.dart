c8b69f56f7c653c4865c1e72ae14baac02db1694
Samir Shaikh <samirshaikh@samirs-MacBook-Pro.local>
2021-02-20 14:32:15 +0530

    mismatch braces bug fix

--- lib/network/news_client.dart
+++ lib/network/news_client.dart
@@ -6,18 +6,17 @@ import 'package:newspaper/data/model/top_headlines.dart';
 
 class NewsClient {
   final _client = Client();
-  
+
   // Fetch Top Headlines from API
   Future<TopHeadlinesModel> fetchTopHeadlines() async {
     final response = await _client.get(
         "${Constants.BASE_URL}/top-headlines?country=us&apiKey=${Constants.API_KEY}");
-            if (response.statusCode == 200) {
+    if (response.statusCode == 200) {
       // If the call to the server was successful, parse the JSON
       return TopHeadlinesModel.fromJson(json.decode(response.body));
     } else {
       // If that call was not successful, throw an error.
       throw Exception('Failed to load post');
     }
-    } 
   }
 }
